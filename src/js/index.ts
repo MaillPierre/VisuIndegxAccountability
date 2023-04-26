edcaedb899827d40b43d6721cf60ab31e039370c
Pierre Maillot <maillpierre@gmail.com>
2023-04-26 17:58:11 +0200

    working eval display (although ugly)

--- src/js/index.ts
+++ src/js/index.ts
@@ -6,7 +6,45 @@ import * as rules from '../computationStructure.json'
 
 const resultFilename = "https://raw.githubusercontent.com/Jendersen/KG_accountability/towardsIndeGx/results/measures_26042023.csv";
 const rawResultTableId = "#rawResults";
-
+const evalResultTableId = "#evalResults";
+const columns = [
+  "Dataset",
+  "Creation date",
+  "Creation location",
+  "Creation methodology",
+  "Creation source",
+  "Aggregated creator contributor",
+  "Maintenance frequency",
+  "Maintenance location",
+  "Maintenance methodology",
+  "Modification date",
+  "Aggregated Maintenance contributor",
+  "Aggregated Usage access",
+  "Usage license",
+  "Usage requirements",
+  "Usage reuse",
+  "Usage concepts covered",
+  "Usage dataset description",
+  "Usage dataset entities",
+  "Usage dataset quality",
+  "Usage RDF serialization",
+  "Usage dataset triples",
+  "Usage End availability",
+  "Usage End validity",
+  "Usage start availability",
+  "Usage access address",
+  "Usage location",
+  "Usage dataset webpage",
+  "Usage audience",
+  "Usage Dataset publisher",
+  "Usage rights"
+];
+const measures = [
+  "Accountability",
+  "Creation",
+  "Maintenance",
+  "Usage"
+]
 
 $(() => {
 
@@ -20,51 +58,37 @@ $(() => {
       skip_empty_lines: true,
       columns: true
     });
-    console.log('data: ', data);
-    let table = $(rawResultTableId).dataTable({
-      data: data,
-      columns: [
-        { "data": "Dataset" },
-        { "data": "Creation date" },
-        { "data": "Creation location" },
-        { "data": "Creation methodology" },
-        { "data": "Creation source" },
-        { "data": "Aggregated creator contributor" },
-        { "data": "Maintenance frequency" },
-        { "data": "Maintenance location" },
-        { "data": "Maintenance methodology" },
-        { "data": "Modification date" },
-        { "data": "Aggregated Maintenance contributor" },
-        { "data": "Aggregated Usage access" },
-        { "data": "Usage license" },
-        { "data": "Usage requirements" },
-        { "data": "Usage reuse" },
-        { "data": "Usage concepts covered" },
-        { "data": "Usage dataset description" },
-        { "data": "Usage dataset entities" },
-        { "data": "Usage dataset quality" },
-        { "data": "Usage RDF serialization" },
-        { "data": "Usage dataset triples" },
-        { "data": "Usage End availability" },
-        { "data": "Usage End validity" },
-        { "data": "Usage start availability" },
-        { "data": "Usage access address" },
-        { "data": "Usage location" },
-        { "data": "Usage dataset webpage" },
-        { "data": "Usage audience" },
-        { "data": "Usage Dataset publisher" },
-        { "data": "Usage rights" }
-      ]
+
+    const evalData = data.map(dataRow => {
+      let dataset = dataRow.Dataset;
+      let datasetEval = evalDataset(dataset);
+      let result = { "Dataset": dataset };
+      measures.forEach(measureName => {
+        result[measureName] = datasetEval.get(measureName);
+      })
+      console.log(result)
+      return result;
     });
 
-    let datasetEvals = new Map();
-    data.forEach((dataRow, index) => {
-      const dataset = dataRow.Dataset;
-      let datasetEval = evalDataset(dataset);
-      datasetEvals.set(dataset, datasetEval);
+    console.log('evalData: ', evalData)
+    let evalDataTable = $(evalResultTableId).dataTable({
+      data: evalData,
+      columns: [{ "data": "Dataset" }].concat(measures.map(columnName => { return { "data": columnName } }))
     });
-    console.log('datasetEvals: ', datasetEvals);
-    console.log('Eval Caligraph: ', datasetEvals.get("http://caligraph.org/.well-known/void").get("Accountability"));
+
+    let rawDataTable = $(rawResultTableId).dataTable({
+      data: data,
+      columns: columns.map(columnName => { return { "data": columnName } })
+    });
+
+    // let datasetEvals = new Map();
+    // data.forEach((dataRow, index) => {
+    //   const dataset = dataRow.Dataset;
+    //   let datasetEval = evalDataset(dataset);
+    //   datasetEvals.set(dataset, datasetEval);
+    // });
+    // console.log('datasetEvals: ', datasetEvals);
+    // console.log('Eval Caligraph: ', datasetEvals.get("http://caligraph.org/.well-known/void").get("Accountability"));
 
     function evalDataset(dataset: string, feature: string = "Accountability"): Map<string, number> {
       let result = new Map<string, number>();
