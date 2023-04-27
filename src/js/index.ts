47ccc3a1c1c717c6119d67b1eef93eb66f401b0e
Pierre Maillot <maillpierre@gmail.com>
2023-04-27 10:45:45 +0200

    missing code

--- src/js/index.ts
+++ src/js/index.ts
@@ -59,35 +59,38 @@ $(() => {
     let currentProfile = profiles[0];
 
     // Generate the raw datatable
-    let rawDataTable = $(rawResultTableId).dataTable({
+    let rawDataTable = $(rawResultTableId).DataTable({
       data: data,
       columns: columns.map(columnName => { return { "data": columnName } })
     });
 
     // Generate the evaluation table data
-    const evalData = data.map(dataRow => {
-      let dataset = dataRow.Dataset;
-      let datasetEval = evalDataset(dataset);
-      let result = { "Dataset": dataset };
-      measures.forEach(measureName => {
-        result[measureName] = datasetEval.get(measureName);
-      })
-      console.log(result)
-      return result;
-    });
+    function generateEvalData(data: any[]) {
+      const evalData = data.map(dataRow => {
+        let dataset = dataRow.Dataset;
+        let datasetEval = evalDataset(dataset);
+        let result = { "Dataset": dataset };
+        measures.forEach(measureName => {
+          result[measureName] = datasetEval.get(measureName);
+        })
+        return result;
+      });
+      return evalData;
+    };
 
     // Generate the evaluated datatable
-    console.log('evalData: ', evalData)
-    let evalDataTable = $(evalResultTableId).dataTable({
-      data: evalData,
+    let evalDataTable = $(evalResultTableId).DataTable({
+      data: generateEvalData(data),
       columns: [{ "data": "Dataset" }].concat(measures.map(columnName => { return { "data": columnName } }))
     });
 
-
+    // Generate the evaluation of a feature from the data, the computation structure and the current profile
     function evalDataset(dataset: string, feature: string = "Accountability", profile = currentProfile): Map<string, number> {
       let result = new Map<string, number>();
       if (rules[feature] !== undefined) {
         let featureScores: number[] = [];
+        // For each child, evaluate it and add it to the result
+        // Childs are nodes that are not leafs from the computation structure
         if (rules[feature].children !== undefined) {
           rules[feature].children.forEach((child: string) => {
             let childEval = evalDataset(dataset, child, profile);
@@ -104,6 +107,8 @@ $(() => {
             featureScores.push(childScore);
           });
         }
+        // For each key, evaluate it and add it to the result
+        // Keys are leaf measure nodes from the data
         if (rules[feature].keys !== undefined) {
           rules[feature].keys.forEach((key) => {
             data.filter(dataRow => dataRow.Dataset === dataset).forEach((dataRow) => {
@@ -125,5 +130,24 @@ $(() => {
         throw new Error(`Feature ${feature} not found in rules`);
       }
     }
+
+    // Connecting the form to the evaluation display
+    // init with current profile
+    Object.keys(currentProfile).forEach(feature => {
+      if (currentProfile[feature].children !== undefined) {
+        currentProfile[feature].children.forEach(child => {
+          const childKey = currentProfile[child.name].key;
+          $(`#${childKey}`).val(child.weight);
+          // on change, update the profile and the evaluation
+          $(`#${childKey}`).on('change', () => {
+            child.weight = Number.parseFloat($(`#${childKey}`).val() as string);
+            evalDataTable.clear()
+            evalDataTable.rows.add(generateEvalData(data))
+            evalDataTable.draw()
+          })
+        });
+      }
+    });
+
   })
 })
