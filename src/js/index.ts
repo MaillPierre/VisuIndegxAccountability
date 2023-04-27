369dcc475cdf4fd3ab11004751f6d77a1454c896
Pierre Maillot <maillpierre@gmail.com>
2023-04-27 15:43:19 +0200

    comment in HTML not displaying

--- src/js/index.ts
+++ src/js/index.ts
@@ -6,6 +6,7 @@ import * as rules from '../computationStructure.json'
 import * as profiles from '../profiles.json'
 
 const resultFilename = "https://raw.githubusercontent.com/Jendersen/KG_accountability/towardsIndeGx/results/measures_26042023.csv";
+const commentFilename = "https://raw.githubusercontent.com/Jendersen/KG_accountability/towardsIndeGx/information_need/metric_question_remarks.csv";
 const rawResultTableId = "#rawResults";
 const evalResultTableId = "#evalResults";
 const columns = [
@@ -51,157 +52,174 @@ $(() => {
 
   // Load the data
   Utils.xhrGetPromise(resultFilename).then((csvContent) => {
-    const data = parse(csvContent, {
+    const data: any[] = parse(csvContent, {
       delimiter: ',',
       skip_empty_lines: true,
       columns: true
     });
-    let currentProfile = profiles[0];
-    $("#standardProfileButton").on("click", () => {
-      if(profiles.find(profile => profile.name === "Standard") !== undefined) {
-        currentProfile = profiles.find(profile => profile.name === "Standard");
-        refreshEvalData();
-        refreshWeights();
-      }
-    });
-    $("#trustProfileButton").on("click", () => {
-      if(profiles.find(profile => profile.name === "Trust") !== undefined) {
-        currentProfile = profiles.find(profile => profile.name === "Trust");
-        refreshEvalData();
-        refreshWeights();
-      }
-    });
-    $("#mostCommonProfileButton").on("click", () => {
-      if(profiles.find(profile => profile.name === "Frequency") !== undefined) {
-        currentProfile = profiles.find(profile => profile.name === "Frequency");
-        refreshEvalData();
-        refreshWeights();
-      }
-    });
-
-    // Generate the raw datatable
-    let rawDataTable = $(rawResultTableId).DataTable({
-      data: data,
-      columns: columns.map(columnName => { return { "data": columnName } })
-    });
-
-    // Generate the evaluation table data
-    function generateEvalData(data: any[]) {
-      const evalData = data.map(dataRow => {
-        let dataset = dataRow.Dataset;
-        let datasetEval = evalDataset(dataset);
-        let result = { "Dataset": dataset };
-        measures.forEach(measureName => {
-          result[measureName] = Utils.precise(datasetEval.get(measureName));
-        })
-        return result;
+    Utils.xhrGetPromise(commentFilename).then((csvCommentContent) => {
+      const commentData: any[] = parse(csvCommentContent, {
+        delimiter: ';',
+        skip_empty_lines: true,
+        columns: true
       });
-      return evalData;
-    };
 
-    // Generate the evaluated datatable
-    let evalDataTable = $(evalResultTableId).DataTable({
-      data: generateEvalData(data),
-      columns: [{ "data": "Dataset" }].concat(measures.map(columnName => { return { "data": columnName } }))
-    });
+      let currentProfile = profiles[0];
+      $("#standardProfileButton").on("click", () => {
+        if (profiles.find(profile => profile.name === "Standard") !== undefined) {
+          currentProfile = profiles.find(profile => profile.name === "Standard");
+          refreshEvalData();
+          refreshWeights();
+        }
+      });
+      $("#trustProfileButton").on("click", () => {
+        if (profiles.find(profile => profile.name === "Trust") !== undefined) {
+          currentProfile = profiles.find(profile => profile.name === "Trust");
+          refreshEvalData();
+          refreshWeights();
+        }
+      });
+      $("#mostCommonProfileButton").on("click", () => {
+        if (profiles.find(profile => profile.name === "Frequency") !== undefined) {
+          currentProfile = profiles.find(profile => profile.name === "Frequency");
+          refreshEvalData();
+          refreshWeights();
+        }
+      });
 
-    // Generate the evaluation of a feature from the data, the computation structure and the current profile
-    // Returns a map of the feature and its children associated with their score
-    function evalDataset(dataset: string, feature: string = "Accountability", profile = currentProfile): Map<string, number> {
-      let result = new Map<string, number>();
-      if (rules[feature] !== undefined) {
-        let featureScores: number[] = [];
-        let weights: number[] = [];
-        // For each child, evaluate it and add it to the result
-        // Childs are nodes that are not leafs from the computation structure
-        if (rules[feature].children !== undefined) {
-          rules[feature].children.forEach((child: string) => {
-            let childEval = evalDataset(dataset, child, profile);
-            // We add the other scores to the result, they are the results of other sub-child computations
-            childEval.forEach((value, key) => {
-              result.set(key, value);
-            });
-            // We get the result of the child computation
-            const rawChildScore = childEval.get(child);
-            // We get the weight of the child in the profile
-            let childWeight = 1;
-            if (profile[feature] !== undefined && profile[feature].children !== undefined && profile[feature].children.findLast(filterChild => filterChild.name === child) !== undefined && profile[feature].children.findLast(filterChild => filterChild.name === child).weight !== undefined) {
-              childWeight = profile[feature].children.findLast(filterChild => filterChild.name === child).weight;
-            }
-            // We multiply the result by its weight for the computation of the feature score
-            const childScore = rawChildScore * childWeight;
-            // We add the weight and the score to the arrays for the computation of the feature score
-            result.set(child, childScore);
-            weights.push(childWeight);
-            featureScores.push(childScore);
-          });
-        }
-        // For each key, evaluate it and add it to the result
-        // Keys are leaf measure nodes from the data
-        if (rules[feature].keys !== undefined) {
-          rules[feature].keys.forEach((key) => {
-            data.filter(dataRow => dataRow.Dataset === dataset).forEach((dataRow) => {
-              // We get the value of the leaf measure in the data
-              const rawLeafValue = Number.parseFloat(dataRow[key]);
-              let leafValue = rawLeafValue;
-              // We get the weight of the leaf measure in the profile
-              let leafWeight = 1;
-              if (profile[feature] !== undefined && profile[feature].keys !== undefined && profile[feature].keys.findLast(filterKey => filterKey.name === key) !== undefined && profile[feature].keys.findLast(filterKey => filterKey.name === key).weight !== undefined) {
-                leafWeight = profile[feature].keys.findLast(filterKey => filterKey.name === key).weight;
-              }
-              // We multiply the value by its weight for the computation of the feature score
-              leafValue = rawLeafValue * leafWeight;
-              // We add the weight and the value to the arrays for the computation of the feature score
-              result.set(key, leafValue);
-              weights.push(leafWeight);
-              featureScores.push(leafValue);
-            });
-          });
-        }
-        // Compute the feature score
-        // We sum the weights and the scores and divide the sum of the scores by the sum of the weights
-        let sumWeights = weights.reduce((a, b) => a + b, 0);
-        let featureScore = featureScores.reduce((a, b) => a + b, 0) / sumWeights;
-        // We add the feature and its score to the result
-        result.set(feature, featureScore);
+      // Generate the raw datatable
+      let rawDataTable = $(rawResultTableId).DataTable({
+        data: data,
+        columns: columns.map(columnName => { return { "data": columnName, "ariaTitle": findComment(columnName) } })
+      });
+      
+      function findComment(measureName) {
+        let resultFind = commentData.filter((commentRow) => commentRow.Measure === measureName)[0];
+        let result = resultFind !== undefined ? resultFind.Advice : measureName;
+        if (resultFind === undefined) {
+          console.log("No comment found for measure " + measureName);
+        } 
         return result;
-      } else {
-        throw new Error(`Feature ${feature} not found in rules`);
       }
-    }
 
-    function refreshEvalData() {
-      evalDataTable.clear()
-      evalDataTable.rows.add(generateEvalData(data))
-      evalDataTable.draw()
-    }
+      // Generate the evaluation table data
+      function generateEvalData(data: any[]) {
+        const evalData = data.map(dataRow => {
+          let dataset = dataRow.Dataset;
+          let datasetEval = evalDataset(dataset);
+          let result = { "Dataset": dataset };
+          measures.forEach(measureName => {
+            result[measureName] = Utils.precise(datasetEval.get(measureName));
+          })
+          return result;
+        });
+        return evalData;
+      };
 
-    function refreshWeights() {
+      // Generate the evaluated datatable
+      let evalDataTable = $(evalResultTableId).DataTable({
+        data: generateEvalData(data),
+        columns: [{ "data": "Dataset" }].concat(measures.map(columnName => { return { "data": columnName ,"ariaTitle": findComment(columnName) } }))
+      });
+
+      // Generate the evaluation of a feature from the data, the computation structure and the current profile
+      // Returns a map of the feature and its children associated with their score
+      function evalDataset(dataset: string, feature: string = "Accountability", profile = currentProfile): Map<string, number> {
+        let result = new Map<string, number>();
+        if (rules[feature] !== undefined) {
+          let featureScores: number[] = [];
+          let weights: number[] = [];
+          // For each child, evaluate it and add it to the result
+          // Childs are nodes that are not leafs from the computation structure
+          if (rules[feature].children !== undefined) {
+            rules[feature].children.forEach((child: string) => {
+              let childEval = evalDataset(dataset, child, profile);
+              // We add the other scores to the result, they are the results of other sub-child computations
+              childEval.forEach((value, key) => {
+                result.set(key, value);
+              });
+              // We get the result of the child computation
+              const rawChildScore = childEval.get(child);
+              // We get the weight of the child in the profile
+              let childWeight = 1;
+              if (profile[feature] !== undefined && profile[feature].children !== undefined && profile[feature].children.findLast(filterChild => filterChild.name === child) !== undefined && profile[feature].children.findLast(filterChild => filterChild.name === child).weight !== undefined) {
+                childWeight = profile[feature].children.findLast(filterChild => filterChild.name === child).weight;
+              }
+              // We multiply the result by its weight for the computation of the feature score
+              const childScore = rawChildScore * childWeight;
+              // We add the weight and the score to the arrays for the computation of the feature score
+              result.set(child, childScore);
+              weights.push(childWeight);
+              featureScores.push(childScore);
+            });
+          }
+          // For each key, evaluate it and add it to the result
+          // Keys are leaf measure nodes from the data
+          if (rules[feature].keys !== undefined) {
+            rules[feature].keys.forEach((key) => {
+              data.filter(dataRow => dataRow.Dataset === dataset).forEach((dataRow) => {
+                // We get the value of the leaf measure in the data
+                const rawLeafValue = Number.parseFloat(dataRow[key]);
+                let leafValue = rawLeafValue;
+                // We get the weight of the leaf measure in the profile
+                let leafWeight = 1;
+                if (profile[feature] !== undefined && profile[feature].keys !== undefined && profile[feature].keys.findLast(filterKey => filterKey.name === key) !== undefined && profile[feature].keys.findLast(filterKey => filterKey.name === key).weight !== undefined) {
+                  leafWeight = profile[feature].keys.findLast(filterKey => filterKey.name === key).weight;
+                }
+                // We multiply the value by its weight for the computation of the feature score
+                leafValue = rawLeafValue * leafWeight;
+                // We add the weight and the value to the arrays for the computation of the feature score
+                result.set(key, leafValue);
+                weights.push(leafWeight);
+                featureScores.push(leafValue);
+              });
+            });
+          }
+          // Compute the feature score
+          // We sum the weights and the scores and divide the sum of the scores by the sum of the weights
+          let sumWeights = weights.reduce((a, b) => a + b, 0);
+          let featureScore = featureScores.reduce((a, b) => a + b, 0) / sumWeights;
+          // We add the feature and its score to the result
+          result.set(feature, featureScore);
+          return result;
+        } else {
+          throw new Error(`Feature ${feature} not found in rules`);
+        }
+      }
+
+      function refreshEvalData() {
+        evalDataTable.clear()
+        evalDataTable.rows.add(generateEvalData(data))
+        evalDataTable.draw()
+      }
+
+      function refreshWeights() {
+        Object.keys(currentProfile).forEach(feature => {
+          if (currentProfile[feature].children !== undefined) {
+            currentProfile[feature].children.forEach(child => {
+              const childKey = currentProfile[child.name].key;
+              $(`#${childKey}`).val(child.weight);
+            });
+          }
+        });
+      }
+
+      // Connecting the form to the evaluation display
+      // init with current profile
+      refreshWeights();
       Object.keys(currentProfile).forEach(feature => {
         if (currentProfile[feature].children !== undefined) {
           currentProfile[feature].children.forEach(child => {
             const childKey = currentProfile[child.name].key;
-            $(`#${childKey}`).val(child.weight);
+            // on change, update the profile and the evaluation
+            $(`#${childKey}`).on('change', () => {
+              child.weight = Number.parseFloat($(`#${childKey}`).val() as string);
+              refreshEvalData()
+            })
           });
         }
       });
-    }
 
-    // Connecting the form to the evaluation display
-    // init with current profile
-    refreshWeights();
-    Object.keys(currentProfile).forEach(feature => {
-      if (currentProfile[feature].children !== undefined) {
-        currentProfile[feature].children.forEach(child => {
-          const childKey = currentProfile[child.name].key;
-          // on change, update the profile and the evaluation
-          $(`#${childKey}`).on('change', () => {
-            child.weight = Number.parseFloat($(`#${childKey}`).val() as string);
-            refreshEvalData()
-          })
-        });
-      }
-    });
-
+    })
   })
 })
